5b3eece7b1a81fcdf075110ca9991f4b7fed10d7
sebastianhoyoslopez <tatansebastan@gmail.com>
2020-12-30 08:25:44 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Comentario from "./components/Comentario.jsx";
+import Saludo from "./components/Saludo.jsx";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container   mt-5">
+      <h1>Proyecto desde cero</h1>
+      <Saludo persona="Ignacio" edad={30} />
+      <Saludo persona="Juanito" edad={28} />
+      <Saludo persona="Pedrito" edad={25} />
+      <hr />
+      <h3>Cajita de comentarios</h3>
+      <Comentario
+        urlImagen="https://picsum.photos/64"
+        persona="Ignacio"
+        texto="fffff"
+      />
+      <Comentario
+        urlImagen="https://picsum.photos/64"
+        persona="Juanito"
+        texto="aaaa"
+      />
+      <Comentario
+        urlImagen="https://picsum.photos/64"
+        persona="Pedrito"
+        texto="bbbb"
+      />
     </div>
   );
 }
---
+++ src/components/Imagen.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Imagen = ({urlImagen}) => {
+  return <img src={urlImagen} className="mr-3" alt="logo" />;
+};
+
+export default Imagen;
